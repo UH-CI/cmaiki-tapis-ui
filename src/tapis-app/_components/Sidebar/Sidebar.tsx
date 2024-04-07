bf9fc3fc3c8aeef9aaaa8135e566308c4dbef4e2
choytr <143041680+choytr@users.noreply.github.com>
2024-04-07 02:11:17 -1000

    Improve collapsing

--- src/tapis-app/_components/Sidebar/Sidebar.tsx
+++ src/tapis-app/_components/Sidebar/Sidebar.tsx
@@ -9,36 +9,51 @@ interface SidebarProps {
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ collapsed }) => {
-  return !collapsed ? (
+  return (
     <div className={styles.root}>
-      <Navbar>
-        <NavItem to="/" icon="dashboard">
-          Dashboard
-        </NavItem>
-        {/*<NavItem to="/systems" icon="data-files">*/}
-        {/*  Systems*/}
-        {/*</NavItem>*/}
-        <NavItem to="/apps" icon="applications">
-          Apps
-        </NavItem>
-        <NavItem to="/files" icon="data-files">
-          Data
-        </NavItem>
-        <NavItem to="/jobs" icon="jobs">
-          Jobs
-        </NavItem>
-        <NavItem to="https://docs.google.com/presentation/d/1UB3VrQXAiCqAJEa0hDQmgmr9TWBlHxPmuo-gtoiRod4/edit#slide=id.g35f391192_00" icon="compass">
-          Walkthrough
-        </NavItem>
-        <NavItem to="mailto:uh-hpc-help@lists.hawaii.edu" icon="conversation">
-          Help
-        </NavItem>
-        {/*<NavItem to="/workflows" icon="publications">*/}
-        {/*  Workflows*/}
-        {/*</NavItem>*/}
-      </Navbar>
+      {collapsed ? (
+        <div className={styles["collapsed"]}>
+          <Navbar>
+            <NavItem to="/" icon="dashboard" />
+            <NavItem to="/apps" icon="applications" />
+            <NavItem to="/files" icon="data-files" />
+            <NavItem to="/jobs" icon="jobs" />
+            <NavItem to="https://docs.google.com/presentation/d/1UB3VrQXAiCqAJEa0hDQmgmr9TWBlHxPmuo-gtoiRod4/edit#slide=id.g35f391192_00" icon="compass" />
+            <NavItem to="mailto:uh-hpc-help@lists.hawaii.edu" icon="conversation" />
+          </Navbar>
+        </div>
+      ) : (
+        <div className={styles["uncollapsed"]}>
+          <Navbar>
+            <NavItem to="/" icon="dashboard">
+              <div className={styles["navitem-text"]}>Dashboard</div>
+            </NavItem>
+            {/*<NavItem to="/systems" icon="data-files">*/}
+            {/*  Systems*/}
+            {/*</NavItem>*/}
+            <NavItem to="/apps" icon="applications">
+              <div className={styles["navitem-text"]}>Apps</div>
+            </NavItem>
+            <NavItem to="/files" icon="data-files">
+              <div className={styles["navitem-text"]}>Data</div>
+            </NavItem>
+            <NavItem to="/jobs" icon="jobs">
+              <div className={styles["navitem-text"]}>Jobs</div>
+            </NavItem>
+            <NavItem to="https://docs.google.com/presentation/d/1UB3VrQXAiCqAJEa0hDQmgmr9TWBlHxPmuo-gtoiRod4/edit#slide=id.g35f391192_00" icon="compass">
+              <div className={styles["navitem-text"]}>Walkthrough</div>
+            </NavItem>
+            <NavItem to="mailto:uh-hpc-help@lists.hawaii.edu" icon="conversation">
+              <div className={styles["navitem-text"]}>Help</div>
+            </NavItem>
+            {/*<NavItem to="/workflows" icon="publications">*/}
+            {/*  Workflows*/}
+            {/*</NavItem>*/}
+          </Navbar>
+        </div>
+      )}
     </div>
-  ) : <></>;
+  );
 };
 
 export default Sidebar;
